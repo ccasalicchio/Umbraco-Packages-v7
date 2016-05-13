5ee53b82faa498329833049ba7c7b98c5d1ac397
Carlos Casalicchio <carlos.casalicchio@arvos.com>
2016-05-13 15:18:20 -0300

    Fixed bug with disabling the button
    Signed-off-by: Carlos Casalicchio <carlos.casalicchio@gmail.com>

--- Custom.Login.Page/js/login.js
+++ Custom.Login.Page/js/login.js
@@ -2,12 +2,12 @@
 	setTimeout(function(){
 		$('button[type="submit"]').on('click',function(){
 			$(this).before("<div class='uil-ring-css' id='processing' style='transform:scale(0.6);'><div></div></div>");
-			$(this).attr('disabled','disabled');
+			//$(this).attr('disabled','disabled');
 		});
 	},800);
 	setInterval(function(){
 		if($('.alert').is(":visible")){
-			$(this).removeAttr('disabled');
+			//$(this).removeAttr('disabled');
 			$('#processing').remove();
 		}
 	},1000);
