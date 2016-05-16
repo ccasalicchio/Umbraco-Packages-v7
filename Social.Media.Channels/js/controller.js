c5bc53ca804ddc82273a2040fd41f049250953e7
Carlos Casalicchio <carlos.casalicchio@arvos.com>
2016-05-16 14:04:56 -0300

    began dev for version 7

---
+++ Social.Media.Channels/js/controller.js
@@ -0,0 +1,22 @@
+﻿angular.module("umbraco")
+    .controller("Social.Media.Channels",
+    function ($scope) {
+
+
+        if (!$scope.model.value) {
+            $scope.model.value = createArray(parseInt($scope.model.config.rows), parseInt($scope.model.config.cols));
+        }
+
+        function createArray(length) {
+            var arr = new Array(length || 0),
+                i = length;
+
+            if (arguments.length > 1) {
+                var args = Array.prototype.slice.call(arguments, 1);
+                while (i > 0) arr[length - 1 - i] = createArray.apply(this, args);
+            }
+
+            return arr;
+        }
+
+    });
