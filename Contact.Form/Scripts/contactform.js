4e8bf1128c700ea66adbf9b4ffff8ce86fbb7fe0
Carlos Casalicchio <carlos.casalicchio@arvos.com>
2016-12-06 23:08:16 -0200

    fixed cpf bug

--- Contact.Form/Scripts/contactform.js
+++ Contact.Form/Scripts/contactform.js
@@ -1,6 +1,6 @@
 $(function () {
 	"use strict";
-	$('input[name="document_cpf"]').mask('000.000.000-00', {reverse: true});
+	$('#document_cpf').mask('000.000.000-00', {reverse: true});
 	
 	var maskBehavior = function (val) {
 			return val.replace(/\D/g, '').length === 11 ? '(00) 00000-0000' : '(00) 0000-00009';
