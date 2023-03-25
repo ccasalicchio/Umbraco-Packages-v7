c2a3bb12d2b287a605bf41b3dc711f21f3726ee8
Carlos A. A. Casalicchio <carlos.casalicchio@gmail.com>
2023-03-24 21:15:59 -0300

    updated icon and assembly

--- Umbraco.Plugins.AdPreview/Properties/AssemblyInfo.cs
+++ Umbraco.Plugins.AdPreview/Properties/AssemblyInfo.cs
@@ -6,9 +6,9 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Umbraco.Plugins.AdPreview")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("SplatDev Ad Preview for Umbraco 7.15.10")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("SplatDev")]
 [assembly: AssemblyProduct("Umbraco.Plugins.AdPreview")]
 [assembly: AssemblyCopyright("Copyright ©  2023")]
 [assembly: AssemblyTrademark("")]
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.0.1")]
+[assembly: AssemblyFileVersion("1.0.0.1")]
